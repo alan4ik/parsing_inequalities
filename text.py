c96a82e5fc8732bb9e19dc71154548dba231df4c
alan4ik <al4ers@gmail.com>
2021-10-20 21:42:58 +0300

    Create text.py

---
+++ text.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+import csv
+import sys
+import codecs
+import re
+import sqlite3
+
+
+def eval_expression(input_string, first, second):
+    allowed_names = {"x": first, "y": second}
+    code = compile(input_string, "<string>", "eval")
+    for name in code.co_names:
+        if name not in allowed_names:
+            raise NameError("Использование %s не разрешено." % name)
+    return eval(code, {"__builtins__": {}}, allowed_names)
+
+
+def parsing_inequalities(func):
+    inequalities = [">=", "<=", "==", ">", "<"]
+    find_inequalities = []
+    arithmetic_operations = ["*", "/", "-", "+"]
+    find_arithmetic_operations = []
+    expression_without_inequality = ''
+
+    for i in inequalities:
+        if func.find(i) != -1:
+            find_inequalities.append(i)
+            expression_without_inequality = func.replace(i, "|||")
+            break
+    for i in inequalities:
+        if expression_without_inequality.find(i) != -1:
+            find_inequalities.append(i)
+            expression_without_inequality = expression_without_inequality.replace(i, "|||")
+            break
+    expression_without_inequality = expression_without_inequality.split("|||")
+
+    expression = ''
+    if len(expression_without_inequality) == 2:
+        expression = expression_without_inequality[0]
+    elif len(expression_without_inequality) == 3:
+        expression = expression_without_inequality[1]
+    else:
+        raise RuntimeError("Error parsing")
+
+    inequality_without_arithmetic_operations = expression
+    for i in arithmetic_operations:
+        if expression.find(i) != -1:
+            find_arithmetic_operations.append(i)
+            expression = expression.replace(i, "|||")
+    expression = expression.split("|||")
+
+    old_expression = ''
+    new_expression = ''
+    if len(expression_without_inequality) == 2:
+        old_expression = new_expression = expression_without_inequality[0]
+    elif len(expression_without_inequality) == 3:
+        old_expression = new_expression = expression_without_inequality[1]
+    else:
+        raise RuntimeError("Error parsing")
+    for i in expression:
+        if new_expression.find(i) != -1:
+            if i.find("y.") != -1:
+                new_expression = new_expression.replace(i, "y")
+            elif i.find("x.") != -1:
+                new_expression = new_expression.replace(i, "x")
+    func = func.replace(old_expression, new_expression)
+    return func
+    # print(func)
+    # int(eval(func, {"x": x, "y": y}))
+
+x = 400
+y = 100
+fun = parsing_inequalities("y.t*y.t+y.t-x.heat_Q>=450")
+print(eval_expression(fun, x, y))
+
+tmp = []
+with open("test.txt") as f:
+    for line in f:
+        tmp.append(line)
+tmp = [line.rstrip() for line in tmp]
+result = []
+for i in tmp:
+    result.append(eval_expression(parsing_inequalities(i), x, y))
+print(result)
